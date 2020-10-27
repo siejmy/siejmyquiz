cfe11c2fdc6a4eb93b4f41776bdd09154b4d5067
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-10-27 16:46:27 +0100

    chore: fix lint errors

--- apps/quiz/src/index.ts
+++ apps/quiz/src/index.ts
@@ -1,12 +1,12 @@
-import Vue from "vue"
+import Vue from 'vue'
 
-import App from '"./App.vue"
-import { Configuration } from '"./Configuration"
+import App from './App.vue'
+import { Configuration } from './Configuration'
 
 Vue.config.productionTip = false
 
 export function mountQuiz(tag: string, _: Configuration) {
   new Vue({
-    render: (h) => h(App),
+    render: h => h(App),
   }).$mount(tag)
 }
