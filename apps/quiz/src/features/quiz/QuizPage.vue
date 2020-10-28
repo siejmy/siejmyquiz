e30821894ba09ba6164de6dcd0ec7daaea508c54
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-10-28 16:45:07 +0100

    feat(quiz): define pages

---
+++ apps/quiz/src/features/quiz/QuizPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="page-quiz">
+    <h1>{{ name }}</h1>
+
+    <ABCDQuizView v-if="type === 'abcd'" :quiz="quiz" />
+    <div v-else>
+      Nie znaleźliśmy odpowiedniego typu quizu. Jeśli chcesz pomóc Siejmy, to
+      kontaktuj się z nami i opowiedz nam o błędzie 😇
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+import { Quiz } from '../../domain'
+
+import { ABCDQuizView } from './abcd'
+
+@Component({
+  components: {
+    ABCDQuizView,
+  },
+})
+export default class QuizPage extends Vue {
+  @Prop({ required: true, type: String })
+  public name!: string
+
+  @Prop({ required: true, type: Object })
+  public quiz!: Quiz
+
+  get type(): string {
+    return this.quiz.type
+  }
+}
+</script>
---
+++ apps/quiz/src/features/results/ResultsPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="page-results">
+    <h1>Results page #{{ resultId }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ResultsPage extends Vue {
+  get resultId(): string {
+    return this.$route.params.id
+  }
+}
+</script>
