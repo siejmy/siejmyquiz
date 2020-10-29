4979744bb4a411a108339bfd95257fe08de51f0c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-10-29 16:53:46 +0100

    feat(quiz): wire facebook app id

--- apps/quiz/src/Configuration.ts
+++ apps/quiz/src/Configuration.ts
@@ -4,14 +4,16 @@ import { QuizABCD, validateQuizABCD } from './domain'
 
 export interface Configuration {
   title: string
-  quiz: QuizABCD
   reCaptchaKey: string
+  facebookAppId: string
+  quiz: QuizABCD
 }
 
 export function validateConfiguration(c: Configuration) {
   ow(c, 'Configuration', ow.object)
   ow(c.title, 'Configuration.title', ow.string.nonEmpty)
   ow(c.reCaptchaKey, 'Configuration.reCaptchaKey', ow.string.nonEmpty)
+  ow(c.facebookAppId, 'Configuration.facebookAppId', ow.string.nonEmpty)
   ow(c.quiz, 'Configuration.quiz', ow.object)
   validateQuizABCD(c.quiz)
 }
