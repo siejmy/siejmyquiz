edecc099f93ea961b8b9ba5aa1f37c7b6aacf5ee
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-15 19:29:13 +0100

    feat: bare for result function

---
+++ functions/src/index.ts
@@ -0,0 +1,13 @@
+import * as functions from "firebase-functions";
+
+export const result = functions
+  .region("us-central1")
+  .runWith({
+    timeoutSeconds: 2,
+    memory: "1GB",
+  })
+  .https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    functions.logger.info(request, { structuredData: true });
+    response.send("Hello from Firebase!");
+  });
